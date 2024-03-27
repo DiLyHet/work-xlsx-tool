56bed8d344d3fa73bd42b1a018f212cee5dadb4f
Ilya Hetmanov <ihetmanov@qualium-systems.com>
2024-03-27 20:37:28 +0200

    changed scripts

---
+++ build/precache-manifest.504bd7c72896e2121325959db8002921.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06a890231f31d9046d88dddc73da8c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b72b5e2402256917acb0",
+    "url": "/static/css/main.6a3c97cd.chunk.css"
+  },
+  {
+    "revision": "2a5db61e2d24c2f1d99b",
+    "url": "/static/js/2.2cdf458a.chunk.js"
+  },
+  {
+    "revision": "ba0bfa8f95d9766194b5",
+    "url": "/static/js/3.7426b29a.chunk.js"
+  },
+  {
+    "revision": "b72b5e2402256917acb0",
+    "url": "/static/js/main.6b06b4df.chunk.js"
+  },
+  {
+    "revision": "3dfe5f0477b66a36e469",
+    "url": "/static/js/runtime~main.a1b9b0b5.js"
+  },
+  {
+    "revision": "6f288899674dbe7a42040c0a54b48d68",
+    "url": "/static/media/dest.6f288899.xlsx"
+  },
+  {
+    "revision": "5e8cc457cf6ee1f0a3aeabf70079f3c7",
+    "url": "/static/media/logo2.5e8cc457.svg"
+  }
+]);
